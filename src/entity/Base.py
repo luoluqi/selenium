9f20319cf501ab4a7f0b3a3e1b6e090989f129f3
luoluqi <944926133@qq.com>
2020-09-16 13:13:00 +0800

    111

--- src/entity/Base.py
+++ src/entity/Base.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 class Base():
     def __init__(self, url):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(r'chromedriver.exe')
         self.driver.maximize_window()
         self.driver.get(url)
         self.driver.implicitly_wait(5)
